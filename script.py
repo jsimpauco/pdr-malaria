cd243aacb6c997c2a2fedb8e24729f120f7e3f44
jsimpauco <jaredsimpauco@Jareds-MacBook-Air.local>
2025-01-22 10:14:55 -0800

    created initial script

---
+++ script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Imports #
+import sys
+import json
+
+# Checking if script.py is being run as a script in command line #
+if __name__ == '__main__':
+
+    # Getting args #
+    args = sys.argv[1:]
