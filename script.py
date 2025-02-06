b8a7fe5b86faf818982ea2388c806cea400d2f26
jsimpauco <jaredsimpauco@Mac.lan>
2025-02-05 19:31:38 -0800

    created environment and updated script

--- script.py
+++ script.py
@@ -9,3 +9,22 @@ if __name__ == '__main__':
 
     # Getting args #
     args = sys.argv[1:]
+
+    # all argument #
+    if 'all' in args:
+
+        print("\n'all' argument given. Running whole script...")
+
+        # Setting args to all available arguments #
+        args = [
+            'data'
+        ]
+    # Other arguments given #
+    else:
+        print('\nArguments given: ' + ', '.join(args))
+        print('\nRunning script based on given arguments...')
+
+    # data argument #
+    if 'data' in args:
+
+        print('\nCurrently running: data.py')
