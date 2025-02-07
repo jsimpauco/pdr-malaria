c2163e087fe195a513acc4389fcfa1fb2a5b92f2
jsimpauco <jaredsimpauco@Jareds-MacBook-Air.local>
2025-02-06 16:14:11 -0800

    added functions to data, still need to create functionality

--- src/data.py
+++ src/data.py
@@ -8,24 +8,28 @@ import os
 import pandas as pd
 from Bio import SeqIO
 
-# Together, these two functions take a large fasta file and create strings of the nucleotide data contained #
-# within into seperate files for each speciemen of malaria included in the file #
+# Together, these two functions take a large fasta file and create #
+# strings of the nucleotide data contained within into seperate files for #
+# each speciemen of malaria included in the file #
 
 # RUNNING THESE WILL CREATE FILES #
 
-def _helper():
-    """
-    Helper function for create_helperdata
-    """
-    
-    filename = 'data/Plasmodium_falciparum_3D7_Genome.fasta'
-
-    # Parses file with genome into dictionary format #
-    record_dict = SeqIO.to_dict(SeqIO.parse(filename, 'fasta'))
-    for key in record_dict.keys():
-        yield record_dict[key].seq, key
-
 def create_helperdata(chunk_size):
+    """
+    TO ADD
+    """
+
+    def _helper():
+        """
+        Helper function for create_helperdata
+        """
+        
+        filename = 'data/Plasmodium_falciparum_3D7_Genome.fasta'
+
+        # Parses file with genome into dictionary format #
+        record_dict = SeqIO.to_dict(SeqIO.parse(filename, 'fasta'))
+        for key in record_dict.keys():
+            yield record_dict[key].seq, key
 
     for sequence, name in iter(_helper()):
         with open(f'data/{name}.txt', 'w') as f:
@@ -34,3 +38,33 @@ def create_helperdata(chunk_size):
                 indx = i*chunk_size
                 chunk = sequence[indx:indx+chunk_size]
                 f.write(f'{chunk}\n')
+
+# Each file contains many instances of strings of #
+# size=chunk_size (default 512), these functions pairs each chunk #
+# with it's neighbors #
+
+def create_pairedData():
+    """
+    TO ADD
+    """
+
+    def create_onepairs(filename):
+        """
+        Helper function for create_pairedData
+        """
+
+        with open(filename) as f:
+            lines = [line.rstrip('\n') for line in f]
+        lines = list(zip(lines[:-1], lines[1:]))
+        return lines
+
+    filenames = []
+    for filename in os.listdir('data'):
+        if '.txt' in filename:
+            f = 'data/' + filename
+            filenames.append(f)
+    pairs = []
+    for temp_file in filenames:
+        pair = create_onepairs(temp_file)
+        pairs = pairs + pair
+    return pairs
